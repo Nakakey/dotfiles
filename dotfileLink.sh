5c75a53b0d1900b8c613443890f722173f39c2bd
中村　圭太 <01027963@CA4854.local>
2019-04-03 13:56:14 +0900

    update

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -1,6 +1,12 @@
 #!bin/sh
-ln -sf ~dotfiles/nvim ~/.config/nvim
-ln -sf ~dotfiles/.latexmkrc ~/.latexmkrc
+# make shimbolic link
+ln -sf ~/dotfiles/nvim ~/.config/nvim
+ln -sf ~/dotfiles/.latexmkrc ~/.latexmkrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/zsh/.tmux.conf ~/.tmux.conf
 
-ln -sf ~dotfiles/zsh/.zshrc ~/.zshrc
-ln -sf ~dotfiles/zsh/.oh-my.zsh ~/.oh-my-zsh
+# clone zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/
